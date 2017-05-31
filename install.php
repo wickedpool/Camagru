1d3c78cb620a5edaabd099a2152657a13ba1745b
Wickedpool <thgiraud@student.42.fr>
2017-05-31 18:31:23 +0200

    install

--- install.php
+++ install.php
@@ -5,12 +5,12 @@ include_once 'db.php';
 try {
 	$DB = explode(';', $DB_DSN);
 	$database = substr($DB[1], 7);
-	$dbh = new PDO("$DB[0]", $DB_USER, $DB_PASSWORD);
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$dbh->exec("CREATE DATABASE IF NOT EXISTS $database");
+	$db = new PDO("$DB[0]", $DB_USER, $DB_PASSWORD);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->exec("CREATE DATABASE IF NOT EXISTS $database");
 	echo "Database '$database' created successfully.<br>";
-	$dbh->exec("use $database");
-	$dbh->exec("CREATE TABLE IF NOT EXISTS membres (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	$db->exec("use $database");
+	$db->exec("CREATE TABLE IF NOT EXISTS membres (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 		email VARCHAR(255) NOT NULL,
 		login VARCHAR(255) NOT NULL,
 		passwd VARCHAR(255) NOT NULL,
@@ -18,21 +18,30 @@ try {
 		admin INT(9) DEFAULT 0,
 		active INT(9) DEFAULT 0)");
 	echo "Table 'membres' created successfully.<br>";
-	$dbh->exec("CREATE TABLE IF NOT EXISTS gallery (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	$db->exec("CREATE TABLE IF NOT EXISTS gallery (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 		login VARCHAR(255) NOT NULL,
 		img VARCHAR(255) NOT NULL)");
 	echo "Table 'gallery' created successfully.<br>";
+	$db->exec("CREATE TABLE IF NOT EXISTS commentaire (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		login VARCHAR(255) NOT NULL,
+		id_image VARCHAR(255) NOT NULL,
+		comment VARCHAR(255) NOT NULL)");
+	echo "Table 'commentaire' created successfully.<br>";
+	$db->exec("CREATE TABLE IF NOT EXISTS jaime (id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+		login VARCHAR(255) NOT NULL,
+		id_image VARCHAR(255) NOT NULL)");
+	echo "Table 'jaime' created successfully.<br>";
 } catch (PDOException $e) {
 	echo $sql.'<br>'.$e->getMessage();
 }
-$dbh = null;
+$db = null;
 ?>
 
 <!DOCTYPE html>
 <html>
   <head>
 	<meta charset="utf-8">
-	<title>Camagru - Setup</title>
+	<title>Camagru</title>
   </head>
   <body>
 	<form action="index.php" class="inline">
